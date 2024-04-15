055a43cbd2b8e65aaaa2ef971143c01f3474a677
Jason <murangira99@gmail.com>
2024-04-15 11:22:38 +0200

    added conecction

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mysql from "mysql2";
 
 
 const app= express ()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Mur@ngi02",
+    database: "test",
+  });
+  
 
 app.listen(8800, () => {
     console.log("Connected to backend.");
