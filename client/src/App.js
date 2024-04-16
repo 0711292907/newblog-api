7ed95de3ba3af8a1988611d4c9ef208dc5a65439
Jason <murangira99@gmail.com>
2024-04-16 09:01:49 +0200

    changed name to books

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
-import Books from "./pages/Home";
+import Books from "./pages/Books";
 import Update from "./pages/Update";
 
 
@@ -9,7 +9,7 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Books />} />
           <Route path="/add" element={<Add />} />
           <Route path="/update/:id" element={<Update />} />
         </Routes>
